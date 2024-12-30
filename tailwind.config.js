fac320bf5d81ba7ef1e2ff47d6fdc6e0cc9882e1
allison-truhlar <amtruhlar@gmail.com>
2024-12-30 15:01:30 -0500

    layout/content: initial layout and content for mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'hero-sm':
-          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('/img/steven-kamenar-MMJx78V7xS8-unsplash-sm.jpg')",
+          // "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('/img/steven-kamenar-MMJx78V7xS8-unsplash-sm.jpg')",
+          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/img/harpers-ferry.jpg')",
+        // "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/img/shenandoah.jpg')",
+        // "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/img/shenandoah2.jpg')",
         'hero-md':
           "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('/img/steven-kamenar-MMJx78V7xS8-unsplash-md.jpg')",
       },
