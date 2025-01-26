708811eaf874d6098c510499fad48f6f00824f25
allison-truhlar <amtruhlar@gmail.com>
2025-01-26 16:14:30 -0500

    style: changes font and fixes responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -6,11 +7,11 @@ module.exports = {
       backgroundImage: {
         'hero-sm':
           // "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('/img/steven-kamenar-MMJx78V7xS8-unsplash-sm.jpg')",
-          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/img/harpers-ferry.jpg')",
+          `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('emily-laing-therapy/img/harpers-ferry.jpg')`,
         // "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/img/shenandoah.jpg')",
         // "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('/img/shenandoah2.jpg')",
         'hero-md':
-          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('/img/steven-kamenar-MMJx78V7xS8-unsplash-md.jpg')",
+          "linear-gradient(rgba(0, 0, 0, 0.6), rgba(8, 45, 56, 0.8)), url('emily-laing-therapy/img/steven-kamenar-MMJx78V7xS8-unsplash-md.jpg')",
       },
     },
   },
